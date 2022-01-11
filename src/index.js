b837d7783d7b65e7236b82f5a127204fe5a9016f
NONP <nonp.znn@gmail.com>
2022-01-11 14:05:36 +0700

    modify graphql js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 let defaultName = "Chi";
+let defaultColor = ["Red","Blue","Green","Yellow","Gold","Silver"];
+
 
 const pubsub = new PubSub();
 
@@ -8,10 +10,13 @@ const typeDefs = `
 	type Query {
 		hello(name: String): String!
 		sayhi: String!
+		today: String!
+		color(colorIndex: Int!): String!
 	}
 
 	type Mutation {
 		changeDefaultName(name: String!): String!
+		changeColor(colorIndex: Int!, colorName: String!): String!
 	}
 
 	type Subscription {
@@ -26,7 +31,20 @@ const resolvers = {
 				name = defaultName;
 			return `Hello World from ${name}!`;
 		},
-		sayhi: () => "Hi API from graphQL"
+		sayhi: () => "Hi API from graphQL",
+		today: () => {
+			const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+			const today = new Date();
+			let day = weekday[today.getUTCDay()];
+			return `Today is ${day}`;
+		},
+		color: (root, { colorIndex }, ctx, info) => {
+			let color = defaultColor[colorIndex-1];
+			if (color)
+				return `Color is ${color}`;
+			else
+				return 'Index out of range';
+		}
 	},
 	Mutation: {
 		changeDefaultName: (root, { name }, ctx, info) => {
@@ -35,7 +53,15 @@ const resolvers = {
 				updateName: `Notify Update Default Name to ${name}`
 			})
 			return `Ok change the default name to ${defaultName}`;
-		}
+		},
+		changeColor: (root, { colorIndex, colorName }, ctx, info) => {
+			if (defaultColor[colorIndex-1]) {
+				defaultColor[colorIndex-1] = colorName;
+				return `Ok change the color index ${colorIndex} to ${colorName}`;
+			}
+			else
+				return 'Index out of range';
+		}		
 	},
 	Subscription: {
 		updateName: {
